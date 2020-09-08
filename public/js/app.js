6ae76b979f29f1a20a749205b6aeef1707d87433
PaKo <pako8128@protonmail.com>
2020-09-09 01:20:14 +0200

    better transition and better questions

--- public/js/app.js
+++ public/js/app.js
@@ -12,12 +12,27 @@ let questions = [
 	{
 		question: "Was ist ein Neutrino?",
 		answer: 0,
-		options: ["Elementarteilchen", "Falsch 1", "Falsch 2", "Falsch 3"]
+		options: ["Elementarteilchen", "Atomkern", "Licht", "Zentrifuge"]
 	},
 	{
 		question: "Was ist ein Neutronenstern?",
 		answer: 2,
-		options: ["Falsch 1", "Falsch 2", "Überreste eines Sterns", "Falsch 3"]
+		options: ["kleinste Sternklasse", "größte Sternklasse", "Überreste eines Sterns", "Atomkernkonfiguration"]
+	},
+	{
+		question: "Was ist Element Nummer 10?",
+		answer: 3,
+		options: ["Sauerstoff", "Helium", "Schwefel", "Neon"]
+	},
+	{
+		question: "Wie ist die Summenformel für Wasserstoffperoxid?",
+		answer: 1,
+		options: ["H20", "H2O2", "H4O2", "H2O3"]
+	},
+	{
+		question: "Wie lang ist ein Jahr auf Merkur?",
+		answer: 2,
+		options: ["53 Tage", "156 Tage", "88 Tage", "12 Jahre"]
 	}
 ];
 let currentQuestion = 0;
@@ -30,16 +45,22 @@ const setQuestion = (n) => {
 	});
 };
 
+const startup = () => {
+	setQuestion(0);
+	main.style.transform = "translateX(0%)";
+};
+
+main.addEventListener("transitionend", () => {
+	setQuestion(currentQuestion);
+	main.style.transform = "translateX(0%)";
+});
+
 options.forEach((opt, i) => {
 	opt.addEventListener("animationend", () => {
-		currentQuestion += 1;
+		currentQuestion = (currentQuestion + 1) % questions.length;
 		opt.classList.remove("correct");
 		opt.classList.remove("wrong");
-
-		main.classList.remove("fade");
-		void main.offsetWidth;
-		main.classList.add("fade");
-		setQuestion(currentQuestion);
+		main.style.transform = "translateX(150%)";
 	});
 
 	opt.addEventListener("click", () => {
