f179dd174ab46bb1e24ace29f265220fd1c787a8
PaKo <pako8128@protonmail.com>
2020-09-08 23:48:40 +0200

    question transition animation

--- public/js/app.js
+++ public/js/app.js
@@ -2,25 +2,51 @@ const categoryImage = document.querySelector(".category img");
 const categoryTitle = document.querySelector(".category h2");
 
 const question = document.querySelector("h1");
-const options = document.querySelectorAll(".option > h3");
+const options = document.querySelectorAll(".option");
+const optionsText = document.querySelectorAll(".option > h3");
 
 const body = document.querySelector("body");
+const main = document.querySelector("main");
 
 let questions = [
 	{
 		question: "Was ist ein Neutrino?",
+		answer: 0,
 		options: ["Elementarteilchen", "Falsch 1", "Falsch 2", "Falsch 3"]
 	},
 	{
 		question: "Was ist ein Neutronenstern?",
-		options: ["Überreste eines Sterns", "Falsch 1", "Falsch 2", "Falsch 3"]
+		answer: 2,
+		options: ["Falsch 1", "Falsch 2", "Überreste eines Sterns", "Falsch 3"]
 	}
 ];
+let currentQuestion = 0;
 
-const setQuestion = (q) => {
+const setQuestion = (n) => {
+	let q = questions[n];
 	question.textContent = q.question;
-	options.forEach((opt, i) => {
+	optionsText.forEach((opt, i) => {
 		opt.textContent = q.options[i];	
 	});
 };
 
+options.forEach((opt, i) => {
+	opt.addEventListener("animationend", () => {
+		currentQuestion += 1;
+		opt.classList.remove("correct");
+		opt.classList.remove("wrong");
+
+		main.classList.remove("fade");
+		void main.offsetWidth;
+		main.classList.add("fade");
+		setQuestion(currentQuestion);
+	});
+
+	opt.addEventListener("click", () => {
+		if (questions[currentQuestion].answer == i) {
+			opt.classList.add("correct");
+		} else {
+			opt.classList.add("wrong");
+		}
+	});
+});
