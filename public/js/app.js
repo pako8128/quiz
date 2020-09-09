53f282b555fe904dd9c066a6249b5dd267db9c51
PaKo <pako8128@protonmail.com>
2020-09-09 13:27:29 +0200

    category list

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-const categoryImage = document.querySelector(".category img");
+const categoryIcon = document.querySelector(".category img");
 const categoryTitle = document.querySelector(".category h2");
 
 const question = document.querySelector("h1");
@@ -8,37 +8,62 @@ const optionsText = document.querySelectorAll(".option > h3");
 const body = document.querySelector("body");
 const main = document.querySelector("main");
 
+const categories = [
+	{
+		name: "Wissenschaft",
+		img: "images/science.svg"
+	},
+	{
+		name: "Filme",
+		img: "images/movie.svg"
+	},
+	{
+		name: "Fernsehen",
+		img: "images/tv.svg"
+	}
+];
+
 let questions = [
 	{
 		question: "Was ist ein Neutrino?",
 		answer: 0,
-		options: ["Elementarteilchen", "Atomkern", "Licht", "Zentrifuge"]
+		options: ["Elementarteilchen", "Atomkern", "Licht", "Zentrifuge"],
+		category: 0
 	},
 	{
 		question: "Was ist ein Neutronenstern?",
 		answer: 2,
-		options: ["kleinste Sternklasse", "größte Sternklasse", "Überreste eines Sterns", "Atomkernkonfiguration"]
+		options: ["kleinste Sternklasse", "größte Sternklasse", "Überreste eines Sterns", "Atomkernkonfiguration"],
+		category: 1
 	},
 	{
 		question: "Was ist Element Nummer 10?",
 		answer: 3,
-		options: ["Sauerstoff", "Helium", "Schwefel", "Neon"]
+		options: ["Sauerstoff", "Helium", "Schwefel", "Neon"],
+		category: 2
 	},
 	{
 		question: "Wie ist die Summenformel für Wasserstoffperoxid?",
 		answer: 1,
-		options: ["H20", "H2O2", "H4O2", "H2O3"]
+		options: ["H20", "H2O2", "H4O2", "H2O3"],
+		category: 0
 	},
 	{
 		question: "Wie lang ist ein Jahr auf Merkur?",
 		answer: 2,
-		options: ["53 Tage", "156 Tage", "88 Tage", "12 Jahre"]
+		options: ["53 Tage", "156 Tage", "88 Tage", "12 Jahre"],
+		category: 1
 	}
 ];
+
 let currentQuestion = 0;
 
 const setQuestion = (n) => {
 	let q = questions[n];
+
+	categoryIcon.src = categories[q.category].img;
+	categoryTitle.textContent = categories[q.category].name;
+
 	question.textContent = q.question;
 	optionsText.forEach((opt, i) => {
 		opt.textContent = q.options[i];	
